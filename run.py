f86980e400d8a6572fb0415bd5da46bd4cc853d6
devemum184 <devemum184@gmail.com>
2026-03-15 19:32:02 +0500

    fix

--- run.py
+++ run.py
@@ -2,10 +2,17 @@ import multiprocessing
 import bot
 import dashboard
 
+
+def run_dash():
+    dashboard.app.run(debug=False, port=8050)
+
+
 if __name__ == '__main__':
     p1 = multiprocessing.Process(target=bot.main)
-    p2 = multiprocessing.Process(target=lambda: dashboard.app.run(debug=False, port=8050))
+    p2 = multiprocessing.Process(target=run_dash)
+
     p1.start()
     p2.start()
+
     p1.join()
     p2.join()
